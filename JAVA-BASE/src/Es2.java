03a56513f6632a48afdc5c8b85fc40d980d98ef0
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-22 16:54:49 +0200

    Corretto esercizio 2

--- JAVA-BASE/src/Es2.java
+++ JAVA-BASE/src/Es2.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class Es2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("inserisci la prima stringa");
+// Proviamo a chiedere all'utente di inserire la prima parola
+        System.out.println("inserisci la prima parola");
         String text1 = scanner.nextLine();
-        System.out.println("inserisci la seconda stringa");
-        String text2 = scanner.nextLine();
+        // Proviamo a chiedere all'utente di inserire la seconda parola
 
-        System.out.println(concatena(text1, text2));
-        System.out.println(concatena(text2, text1));
+        System.out.println("inserisci la seconda parola");
+        String text2 = scanner.nextLine();
+        // Proviamo a chiedere all'utente di inserire la terza parola
+        System.out.println("inserisci la terza parola");
+        String text3 = scanner.nextLine();
+
+        System.out.println(concatena(text1, text2, text3));
+        System.out.println(concatena(text3, text2, text1));
 
         scanner.close();
     }
 
-    public static String concatena(String text1, String text2) {
-        return text1 + " " + text2;
+    public static String concatena(String text1, String text2, String text3) {
+        return text1 + " " + text2 + " " + text3;
     }
 }
