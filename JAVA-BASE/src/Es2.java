39e94f6cf16036a76ab5b788b1a714cf3bd00277
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-22 16:48:50 +0200

    completato esercizio 2

--- JAVA-BASE/src/Es2.java
+++ JAVA-BASE/src/Es2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Es2 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("inserisci la prima stringa");
+        String text1 = scanner.nextLine();
+        System.out.println("inserisci la seconda stringa");
+        String text2 = scanner.nextLine();
+
+        System.out.println(concatena(text1, text2));
+        System.out.println(concatena(text2, text1));
+
+        scanner.close();
+    }
+
+    public static String concatena(String text1, String text2) {
+        return text1 + " " + text2;
     }
 }
